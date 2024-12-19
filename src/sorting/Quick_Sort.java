c8acf5d720b9649056b2431d834c416c65ec0fa2
lucky <bandhey3@gmail.com>
2024-12-19 23:58:38 +0530

    push sorting concepts

--- src/sorting/Quick_Sort.java
+++ src/sorting/Quick_Sort.java
@@ -1,3 +1,34 @@
+/*
+
+Implement Quick Sort, a Divide and Conquer algorithm, to sort an array, arr[] in ascending order. Given an array, arr[], with starting index low and ending index high, complete the functions partition() and quickSort(). Use the last element as the pivot so that all elements less than or equal to the pivot come before it, and elements greater than the pivot follow it.
+
+Note: The low and high are inclusive.
+
+Examples:
+
+Input: arr[] = [4, 1, 3, 9, 7]
+Output: [1, 3, 4, 7, 9]
+Explanation: After sorting, all elements are arranged in ascending order.
+Input: arr[] = [2, 1, 6, 10, 4, 1, 3, 9, 7]
+Output: [1, 1, 2, 3, 4, 6, 7, 9, 10]
+Explanation: Duplicate elements (1) are retained in sorted order.
+Input: arr[] = [5, 5, 5, 5]
+Output: [5, 5, 5, 5]
+Explanation: All elements are identical, so the array remains unchanged.
+Constraints:
+1 <= arr.size() <= 103
+1 <= arr[i] <= 104
+
+Time Complexity
+Best, Average, Worst Case:  𝑂 ( 𝑛 log ⁡ 𝑛 ) O(nlogn)
+The array is divided recursively into halves ( log ⁡ 𝑛 logn) and merged linearly ( 𝑂 ( 𝑛 ) O(n)).
+
+Space Complexity
+𝑂 ( 𝑛 ) O(n) due to the temporary storage for merging.
+
+
+ */
+
 package sorting;
 
 import java.util.Arrays;
@@ -5,6 +36,43 @@ import java.util.Arrays;
 public class Quick_Sort {
     public static void main(String[] args) {
         int[] arr = {2, 4, 6, 3, 6, 1};
+        quickSort(arr, 0, arr.length - 1);
         System.out.println(Arrays.toString(arr));
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pIndex = partition(arr, low, high);
+            quickSort(arr, low, pIndex - 1);
+            quickSort(arr, pIndex + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[low];
+        int i = low + 1;
+        int j = high;
+
+        while (i <= j) {
+            while (i <= high && arr[i] <= pivot) {
+                i++;
+            }
+            while (j >= low && arr[j] > pivot) {
+                j--;
+            }
+
+            if (i < j) {
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, low, j);
+        return j;
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 }
